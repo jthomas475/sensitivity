20e2ef5f776512c4e9cf70967a3ff8dd9d9391e0
jthomas475 <jordan.thomas1623@gmail.com>
2026-05-02 16:56:19 -0400

    Updated failure handling, expanded analysis
    Instead of reverting to original, unperturbed data upon simulation
    failure, I set the failed indices of the matrix to nan and then filtered
    afterwards. Also expanded analysis to include EDV, ESV, STROKE, and EF.

--- sensitivity.py
+++ sensitivity.py
@@ -10,6 +10,7 @@ import copy
 from SALib.sample import sobol as sobol_sample
 from SALib.analyze import sobol
 from SALib.test_functions import Ishigami
+from SALib import ProblemSpec
 
 # Need to establish test cases and error tests
 
@@ -64,7 +65,9 @@ def change_res(data, scaler, ves_name):
 
 def get_p_metric(data, metric):
     results = zerod.simulate(data)
-    pressure = np.array(results[results.name == "pressure:outlet_valve:downstream_vessel"].y) # Accesses LV Pressure
+    # pressure = np.array(results[results.name == "pressure:outlet_valve:downstream_proximal_vessel"].y) # Accesses Caruel Pressure for proximal vessel
+    pressure = np.array(results[results.name == "pressure:outlet_valve:downstream_vessel"].y) # Accesses LV Pressure 
+
     if(metric == "max"):
         p_max = np.max(pressure)
         return p_max
@@ -76,7 +79,64 @@ def get_p_metric(data, metric):
     elif(metric == "mean"):
         p_mean = np.mean(pressure)
         return p_mean
+    
 
+def get_radius(data):
+    for ves in data["vessels"]:
+        if ves["zero_d_element_type"]=="ChamberSphere":
+            return ves["zero_d_element_values"]["radius0"]
+
+def get_v_metric(data, metric):
+    results = zerod.simulate(data)
+
+    LV_radius = get_radius(data)
+
+    vol = np.array(results[results.name == "volume:ventricle"].y) + LV_radius
+    time = np.array(results[results.name == "volume:ventricle"].time)
+
+    if(metric == "max"):
+        p_max = np.max(vol)
+        return p_max
+    
+    elif(metric == "min"):
+        p_min = np.min(vol)
+        return p_min
+    
+    elif(metric == "mean"):
+        p_mean = np.mean(vol)
+        return p_mean
+
+
+
+def get_f_metric(data, metric):
+    results = zerod.simulate(data)
+    flow = np.array(results[results.name == "flow:outlet_valve:downstream_vessel"].y) # Accesses LV Pressure
+    if(metric == "max"):
+        f_max = np.max(flow)
+        return f_max
+    
+    elif(metric == "min"):
+        f_min = np.min(flow)
+        return f_min
+    
+    elif(metric == "mean"):
+        f_mean = np.mean(flow)
+        return f_mean
+
+# Note that end-diastolic volume (EDV) is 100% of the max volume (so just max volume)
+# End-systolic volume (ESV) is x% of the max volume (about 30%-40% max volume)
+# stroke volume is EDV-ESV
+# Ejection fraction is stroke Volume / EDV * 100
+def get_outputs(data):
+    EDV = get_v_metric(data,"max")
+
+    ESV = get_v_metric(data,"min")
+
+    stroke = EDV-ESV
+
+    EF = stroke/EDV *100
+
+    return EDV, ESV, stroke, EF
 
 
 
@@ -162,18 +222,18 @@ def create_dict(json_file):
                 "params": params
             }
 
-    for valve in json_file["valves"]:
-        name = valve["name"]
-        params = {}
+    # for valve in json_file["valves"]:
+    #     name = valve["name"]
+    #     params = {}
 
-        for k, v in valve["params"].items():
-            if isinstance(v, (int, float)):
-                params[k] = v
-        if params:
-            param_dict[name] = {
-                "type": "valve",
-                "params": params
-            }
+    #     for k, v in valve["params"].items():
+    #         if isinstance(v, (int, float)):
+    #             params[k] = v
+    #     if params:
+    #         param_dict[name] = {
+    #             "type": "valve",
+    #             "params": params
+    #         }
 
     return param_dict
 
@@ -203,7 +263,7 @@ def create_indicators(param_dict):
 # Parameters:
 # 
 def evaluate_model(data, params, param_map, metric):
-    Y = np.zeros(len(params))
+    p_maxs, EDV, ESV, stroke, EF = [np.full(len(params), np.nan) for _ in range(5)]
     failures = 1
 
     # index i, element X (which is a singular row of column vector params)
@@ -230,7 +290,9 @@ def evaluate_model(data, params, param_map, metric):
                         valve["params"][param] *= scaler
 
         try:
-            Y[i] = get_p_metric(perturbed_data, metric)
+            p_maxs[i] = get_p_metric(perturbed_data, metric)
+            EDV[i], ESV[i], stroke[i], EF[i] = get_outputs(perturbed_data)
+
 
         except RuntimeError as e:
             print(f"Failure numer: {failures}")
@@ -239,10 +301,10 @@ def evaluate_model(data, params, param_map, metric):
             for ves, param, scaler in failed_params:
                 print(f" {ves}-{param}: {scaler}")
             print(f"Error message: {e}")
-            failures+=1
-            Y[i] = get_p_metric(data, metric)
 
-    return Y
+            failures+=1
+
+    return p_maxs, EDV, ESV, stroke, EF
 
 
 # Function that measures sensitivity of pressure (could generalize to allow user to decide - create parameter to allow this eventually) 
@@ -265,31 +327,53 @@ def sobol_sensitivity(data, param_dict, bound, metric):
     problem = {
         'num_vars': num_vars,
         'names': param_indicators,
-        'bounds': [bound]*num_vars # note these are arbitrarily chosen, not physically grounded - not sure if the range is too small
+        'bounds': [bound]*num_vars, # note these are arbitrarily chosen, not physically grounded - not sure if the range is too small
+        'outputs': ["p_maxs","EDV","ESV","stroke","EF"]
     }
 
     # Using 512 as arbitrary sample size (for greater accuracy, could 
     # double or quadurple sample size). Want to keep sample size power of 2
-    # This step generates the input (sobol) parameter set
+    # This step generates the set of model inputs
     # Total # of model evaluations will be N (2*NV+2), where NV is number of variables (given NV>1)
-    params = sobol_sample.sample(problem, 512, calc_second_order=True) 
+    params = sobol_sample.sample(problem, 1024, calc_second_order=True) 
 
-    Y = evaluate_model(data, params, param_map, metric)
+    p_maxs, EDV, ESV, stroke, EF = evaluate_model(data, params, param_map, metric)
+
+    valid_rows = ~(np.isnan(p_maxs) | np.isnan(EDV) | np.isnan(ESV) | np.isnan(stroke) | np.isnan(EF))
+    clean_p_maxs = p_maxs[valid_rows]
+    clean_EDV = EDV[valid_rows]
+    clean_ESV = ESV[valid_rows]
+    clean_stroke = stroke[valid_rows]
+    clean_EF = EF[valid_rows]
+
+
+    failure_rate = (~valid_rows).sum() / len(valid_rows)
+    print(f"Failure rate: {failure_rate}")
+
+
+    if failure_rate > 0.05:
+        print("WARNING: Failure rate > 0.05. Sensitivity indices may be unreliable.")
+
 
     # Provides insight into how much each parameter contributes to output variance
     # Essentially the actual evaluation step where the sobol variance-based
     # sensitivty analysis formula described in Stelli is evaluated.
-    Si = sobol.analyze(problem, Y, calc_second_order=True, print_to_console=True)
-
-    return Si
+    Si_p_maxs = sobol.analyze(problem, clean_p_maxs, calc_second_order=True, print_to_console=True)
+    Si_EDV = sobol.analyze(problem, clean_EDV, calc_second_order=True, print_to_console=True)
+    Si_ESV = sobol.analyze(problem, clean_ESV, calc_second_order=True, print_to_console=True)
+    Si_stroke = sobol.analyze(problem, clean_stroke, calc_second_order=True, print_to_console=True)
+    Si_EF = sobol.analyze(problem, clean_EF, calc_second_order=True, print_to_console=True)
+    
+    return Si_p_maxs, Si_EDV, Si_ESV, Si_stroke, Si_EF, problem["outputs"]
 
 
 ####################
 # Heatmap Function #
 ####################
-def sobol_heatmap(Si, index, param_dict):
+def s2_heatmap(Si, param_dict):
     param_indicators, param_map = create_indicators(param_dict)
 
+    index = "S2"
     Si_index_matrix = np.abs(Si[index])
          
     plt.figure(figsize=(10,8))
@@ -304,24 +388,76 @@ def sobol_heatmap(Si, index, param_dict):
     plt.tight_layout()
     plt.show()
 
+# @param Si_s: Array of sobol analysis results
+# @ param: names: The names of the outputs
+# @param param_dict: dictionary of the parameters
+# @param names: array of sensitivty analysis value/output names (["Y","EDV","ESV","stroke","EF"])
+def SA_heatmap(Si_s, names, param_dict):
+    #pdb.set_trace()
+
+    param_indicators, param_map = create_indicators(param_dict)
+    indices = ["ST","S1"]
+
+    mymap = []
+
+    for sobol_idx in indices:
+        for i, Si in enumerate(Si_s):
+            Si_index_matrix = np.array(Si[sobol_idx]) # an array containing all the sobol values for given sobol_idx
+
+            Si_index_matrix[Si_index_matrix < 0] = 0
+
+
+            mymap.append(Si_index_matrix)
+    
+
+    half = int(len(mymap) / 2) 
+    heatmap_ST = np.array(mymap[:half]).T
+    heatmap_SI = np.array(mymap[half:]).T
+
+    # Print heatmap for ST
+    plt.figure(figsize=(10,8))
+    plt.imshow(heatmap_ST)
+
+    plt.yticks(range(len(param_indicators)), param_indicators)
+    plt.xticks(range(len(names)), names, rotation=90)
+
+    plt.colorbar(label=f"ST")
+    plt.title(f"Sobol ST sensitivity analysis heatmap")
+
+    plt.tight_layout()
+    plt.show()   
+
+    # Print heatmap for SI
+    plt.figure(figsize=(10,8))
+    plt.imshow(heatmap_SI)
+
+    plt.yticks(range(len(param_indicators)), param_indicators)
+    plt.xticks(range(len(names)), names, rotation=90)
+
+    plt.colorbar(label=f"SI")
+    plt.title(f"Sobol SI sensitivity analysis heatmap")
+
+    plt.tight_layout()
+    plt.show()   
+
 ####################
 # End of functions #
 ####################
 
 
-LV_fname = "/mnt/c/Users/jorda/OneDrive/Desktop/School Stuff/Yale Computational Biomechanics Research/Computational Biomechanics - svzerodsolver repo/svZeroDSolver-jt/tests/cases/chamber_sphere.json"
+LV_fname = "/mnt/c/Users/jorda/OneDrive/Desktop/School Stuff/Yale Computational Biomechanics Research/Computational Biomechanics - svzerodsolver repo/sensitivity/chamber_sphere.json"
 
 LV_baseline_input = read_file(LV_fname)
-LV_baseline_input["simulation_parameters"]["number_of_cardiac_cycles"] = 2
+# LV_baseline_input["simulation_parameters"]["number_of_cardiac_cycles"] = 2
 LV_baseline_input["simulation_parameters"]["output_all_cycles"] = False
 
 
 
-Caruel_fname = "/mnt/c/Users/jorda/OneDrive/Desktop/School Stuff/Yale Computational Biomechanics Research/Computational Biomechanics - svzerodsolver repo/sensitivity/chamber_sphere_Caruel.json"
+# Caruel_fname = "/mnt/c/Users/jorda/OneDrive/Desktop/School Stuff/Yale Computational Biomechanics Research/Computational Biomechanics - svzerodsolver repo/sensitivity/chamber_sphere_Caruel.json"
 
-Caruel_baseline_input = read_file(Caruel_fname)
-Caruel_baseline_input["simulation_parameters"]["number_of_cardiac_cycles"] = 2
-Caruel_baseline_input["simulation_parameters"]["output_all_cycles"] = False
+# Caruel_baseline_input = read_file(Caruel_fname)
+# Caruel_baseline_input["simulation_parameters"]["number_of_cardiac_cycles"] = 2
+# Caruel_baseline_input["simulation_parameters"]["output_all_cycles"] = False
 
 # pdb.set_trace() # review documentation (placed here in order to analyze baseline_
                 #results.name)
@@ -341,13 +477,18 @@ bound = [0.8,1.2]
 
 LV_dict = create_dict(LV_baseline_input)
 
-Caruel_dict = create_dict(Caruel_baseline_input)
+# Caruel_dict = create_dict(Caruel_baseline_input)
 
-Si_LV = sobol_sensitivity(LV_baseline_input, LV_dict, bound, "max")
+Si_y, Si_EDV, Si_ESV, Si_stroke, Si_EF, output_names = sobol_sensitivity(LV_baseline_input, LV_dict, bound, "max")
 
-# Si_Caruel = sobol_sensitivity(Caruel_baseline_input, Caruel_dict, bound, "max")
+# #Si_Caruel = sobol_sensitivity(Caruel_baseline_input, Caruel_dict, bound, "max")
 
-sobol_heatmap(Si_LV, "S2", LV_dict)
-# heatmap(Si_LV, "S1", LV_dict)
-# heatmap(Si_LV, "ST", LV_dict)
-print(f"S2 values:\n {Si_LV[S2]}")
+LV_SIs = [Si_y, Si_EDV, Si_ESV, Si_stroke, Si_EF]
+
+# np.save("./LV_SIs",LV_SIs)
+np.save("./LV_SIs_1024",LV_SIs)
+#LV_SIs = np.load("./LV_SIs.npy", allow_pickle=True)
+
+SA_heatmap(LV_SIs, ["Max Pressure","EDV","ESV","STROKE","EF"], LV_dict)
+# # print(f"S2 values:\n {Si_LV[S2]}")
+
